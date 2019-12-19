cffc72513a25704ca7dfc2650f57e2f83ea0ae57
Zhuanle <1847496393@qq.com>
2019-12-19 11:34:22 +0800

    springbootmybatis.4

---
+++ src/main/java/com/example/springbootmybatis/configuration/PageHelperConfiguration.java
@@ -0,0 +1,19 @@
+package com.example.springbootmybatis.configuration;
+
+import com.github.pagehelper.PageHelper;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.Properties;
+
+@Configuration public class PageHelperConfiguration {
+  @Bean public PageHelper pageHelper() {
+    PageHelper pageHelper = new PageHelper();
+    Properties p = new Properties();
+    p.setProperty("offsetAsPageNum", "true");
+    p.setProperty("rowBoundsWithCount", "true");
+    p.setProperty("reasonable", "true");
+    pageHelper.setProperties(p);
+    return pageHelper;
+  }
+}
--- src/main/java/com/example/springbootmybatis/controller/TestController.java
+++ src/main/java/com/example/springbootmybatis/controller/TestController.java
@@ -2,22 +2,30 @@ package com.example.springbootmybatis.controller;
 
 import com.example.springbootmybatis.mapper.TestMapper;
 import com.example.springbootmybatis.pojo.User1;
+import com.github.pagehelper.PageHelper;
+import com.github.pagehelper.PageInfo;
 import org.springframework.beans.factory.annotation.Autowired;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
+/**
+ *
+ */
 
 
-@RestController public class TestController {
+@Controller public class TestController {
 
   @Autowired TestMapper testMapper;
 
   @RequestMapping("/add") public void add() {
     User1 test = new User1();
-    test.setId(10);
+    test.setId(12);
     test.setName("zw");
     test.setUsername("w");
     test.setPassword("123456");
@@ -26,8 +34,13 @@ import java.util.List;
 
   }
 
-  @RequestMapping("/list") public List<User1> list() {
+  @RequestMapping("/list")
+  public String list(Model m, @RequestParam(value = "start", defaultValue = "0") int start,
+      @RequestParam(value = "size", defaultValue = "5") int size) {
+    PageHelper.startPage(start, size);
     List<User1> list = testMapper.listTest();
-    return list;
+    PageInfo<User1> page = new PageInfo<>(list);
+    m.addAttribute("page", page);
+    return "user";
   }
 }
